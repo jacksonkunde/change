320166d874522ae5e279971224103da0356fab9d
jacksonkunde <54416292+jacksonkunde@users.noreply.github.com>
2020-09-03 13:47:43 -0700

    add a change file that does absolutely nothing

---
+++ change.java
@@ -0,0 +1,16 @@
+public class change {
+
+	public static void main(String[] args)
+	{
+		System.out.println("hello, world!");
+	}
+	//count the number of ways to make change for a given amount of money
+
+	public static int NumberOfWaysToMakeChange(int cents)
+	{
+		if(cents < 5)
+		{
+			return 1;
+		}
+	}
+}
